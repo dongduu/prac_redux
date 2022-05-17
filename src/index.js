7f517569bafa501f8b427acbb0ae42e08ffa0ccd
dongduu <4012popo@naver.com>
2022-05-17 16:27:25 +0900

    feat: use store & reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
+import { createStore } from "redux"; // data를 넣는 곳, state
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0) => {
+  return count;
 };
 
-const handelAdd = () => {
-  count++;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-const handelMinus = () => {
-  count--;
-  updateText();
-};
-
-add.addEventListener("click", handelAdd);
-minus.addEventListener("click", handelMinus);
+console.log(countStore.getState());
