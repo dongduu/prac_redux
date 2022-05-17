70e88492d06af689cc1b0f468f0532a0083ec4dd
dongduu <4012popo@naver.com>
2022-05-18 00:29:04 +0900

    feat: use subscribe

--- src/index.js
+++ src/index.js
@@ -16,7 +16,21 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
 
 console.log(countStore.getState());
