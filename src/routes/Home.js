c9f8f4fe1d53f9920d77884567d79dd2d0fb1c51
dongduu <4012popo@naver.com>
2022-05-23 17:30:12 +0900

    feat: mapDispatchToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home({ toDos }) {
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
+    addToDo(text);
     setText("");
   }
   return (
@@ -23,10 +25,18 @@ function Home({ toDos }) {
 }
 
 function mapStateToProps(state) {
+  // store.getState()
   // 뭔가를 store에서 가져오고 싶을 떄 사용 => 가져와서 component의 props에 넣음
   // 첫 번째 argument: store에서 state를 가져옴
   // 두 번째 argument: component의 props
   return { toDos: state };
 }
 
-export default connect(mapStateToProps)(Home); // 두 개의 argument를 가지는 connect(state, dispatch)
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home); // 두 개의 argument를 가지는 connect(state, dispatch)
+// export default connect(null, mapDispatchProps)(Home) => mapState는 필요하지 않고 dispatch는 필요할 때
