9d00e48b57b9fa4c06a167fd8260a86b7515a8db
dongduu <4012popo@naver.com>
2022-05-30 17:25:43 +0900

    feat: add Detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,19 @@
-export default () => "Detail";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+function Detail() {
+  const id = useParams().id;
+  const toDos = useSelector((state) => state);
+  console.log(toDos);
+  const toDoText = toDos.find((toDo) => toDo.id === parseInt(id));
+  console.log(toDoText);
+  return (
+    <>
+      <h1>{toDoText?.text}</h1>
+      <h5>Created at : {toDoText?.id}</h5>
+    </>
+  );
+}
+
+export default Detail;
